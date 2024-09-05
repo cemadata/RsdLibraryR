a46778d8fe376892572935326fa743139d7a47b0
cemadata <cemadata@rennes.unicancer.fr>
2024-09-05 12:01:44 +0200

    Table de survie globale mise à jour

--- R/biostats_fonctiontabsurvie.R
+++ R/biostats_fonctiontabsurvie.R
@@ -1,16 +1,14 @@
-##################################
-##### Fonction table survie #####
-#################################
-
-biostats_fonctiontabsurvie <- function(dat, duree_obs, deces){
-  fit.gp <- survfit(Surv(duree_obs, deces) ~ ARMCD, data = dat, conf.type = "log-log")
-  surv.res <- matrix(paste(round(summary(fit.gp, time = c(6,12,24))$surv,3)*100, "% [", round(summary(fit.gp, time = c(6,12,24))$lower,3)*100, " - ",
-                           round(summary(fit.gp, time = c(6,12,24))$upper,3)*100, "]", sep = ""),
-                     nrow = 3, ncol = 2, byrow = F,
-                     dimnames = list(c("Survival rate at M6","Survival rate at M12","Survival rate at M24"),levels(dat$ARMCD)))
+# Fonction pour créer la table de survie
+biostats_fonctiontabsurvie <- function(dat, duree_obs, deces, gp){
+  fit.gp <-  survival::survfit(survival::Surv(duree_obs, deces) ~ gp, data = dat, conf.type = "log-log")
+  surv.res <- matrix(paste(round(summary(fit.gp, time = c(6,12,24,60))$surv,2)*100, "% [", round(summary(fit.gp, time = c(6,12,24,60))$lower,2)*100, " - ",
+                           round(summary(fit.gp, time = c(6,12,24,60))$upper,2)*100, "]", sep = ""),
+                     nrow = 4, ncol = length(levels(gp)), byrow = F,
+                     dimnames = list(c("Survival rate at M6","Survival rate at M12","Survival rate at M24", "Survival rate at M60"),paste(levels(gp))))
   os <- surv_median(fit.gp)
+
   surv.res <- rbind(surv.res, paste(round(os$median,1), " months [", round(os$lower,1), " - ", round(os$upper,1), "]", sep = ""))
   surv.res <- as.data.frame(surv.res)
-  rownames(surv.res)[4] <- "Median overall survival"
+  rownames(surv.res)[5] <- "Median OS"
   surv.res <- as.data.frame(surv.res)
   return(surv.res)}
